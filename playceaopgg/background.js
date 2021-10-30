2f632d82a96e955ab3a69b5bb15d933bf487d591
daviditsygin <daviditsygin@users.noreply.github.com>
2021-10-30 15:01:03 -0700

    Add changing icon

--- playceaopgg/background.js
+++ playceaopgg/background.js
@@ -3,6 +3,29 @@ function getopgg() {
   return `https://na.op.gg/multi/query=${encodeURIComponent(summonerNames.join(", "))}`;
 }
 
+const changeListener = async () => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  if (tab.url.indexOf('https://app.playcea.com/teams/') > -1) {
+    chrome.action.setIcon(
+      { path: 'images/icon-clickable.png' }
+    )
+  }
+  else if (tab.url.indexOf('https://app.playcea.com/') > -1) {
+    chrome.action.setIcon(
+      { path: 'images/icon-active.png' }
+    )
+  }
+  else {
+    chrome.action.setIcon(
+      { path: 'images/icon-inactive.png' }
+    )
+  }
+}
+
+chrome.webNavigation.onDOMContentLoaded.addListener(changeListener)
+chrome.tabs.onActivated.addListener(changeListener)
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get({
     clickbehavior: 'tab',
