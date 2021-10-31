0f44425f84690b6d54571394e84430df5b8bb3aa
daviditsygin <daviditsygin@users.noreply.github.com>
2021-10-31 09:18:46 -0700

    Add script to check for summoner names

--- playceaopgg/background.js
+++ playceaopgg/background.js
@@ -3,6 +3,19 @@ function getopgg() {
   return `https://na.op.gg/multi/query=${encodeURIComponent(summonerNames.join(", "))}`;
 }
 
+function checkSummonerNames() {
+  const iconForSummonerNames = 'M12 14v8H4a8 8 0 0 1 8-8zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm9.446 7.032l1.504 1.504-1.414 1.414-1.504-1.504a4 4 0 1 1 1.414-1.414zM18 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'
+  const iconForCeaNames = 'M12 14v2a6 6 0 0 0-6 6H4a8 8 0 0 1 8-8zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm9.446 9.032l1.504 1.504-1.414 1.414-1.504-1.504a4 4 0 1 1 1.414-1.414zM18 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'
+  const elems = document.querySelectorAll(`path[d='${iconForCeaNames}']`)
+  if (elems.length > 0) {
+    alert('You must click the icon outlined in red to enable summoner names. Try again after enabling.')
+    elems[0].parentElement.parentElement.parentElement.style.border = '4px solid red';
+    return false
+  }
+  document.querySelectorAll(`path[d='${iconForSummonerNames}']`)[0].parentElement.parentElement.parentElement.style.border = '';
+  return true
+}
+
 const changeListener = async () => {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
